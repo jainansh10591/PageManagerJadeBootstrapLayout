1459fb6377197f3faa723bb89683c0bc276751f0
Anshul  Kataria <jainansh10591@gmail.com>
2015-12-31 23:42:28 -0500

    Incomplete form validation.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,53 @@
-function formSubmission(element){
-	$(".loader").addClass( "loader-active" );
+// function formSubmission(e){
+// 	//$(".loader").addClass( "loader-active" );
+// 	var eve = e;
+// 	var form = $("#newPostForm");
+// 	var temp;
+// };
+
+function formSubmission(element, event){
+	//$(".loader").addClass( "loader-active" );
+	var valid = true;
+	var formType = element.elements["type"].value;
+	if(formType == "status"){
+		
+	}
+	else if(formType == "link"){
+		var link = element.elements["link"];
+		if(link.value.trim() == ""){
+			valid = false;
+		}
+		if(!isUrlValid(link.value.trim())){
+			valid = false;
+		}
+	}
+	else if(formType == "photo" || formType == "video" ){
+		var source = element.elements["source"];
+		var url = element.elements["url"];
+		if(source){
+			var value = source.value;
+		}
+		if(url){
+			// also check that url is of photo/video or not
+			if(!isUrlValid(url.value.trim())){
+				valid = false;
+			}	
+		}
+		
+	}
+	else{
+		
+	}
+	if(!valid){
+		event.preventDefault();
+	}else{
+		$(".loader").addClass( "loader-active" );
+	}
+	
+
+};
+function isUrlValid(url) {
+    return /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
 };
 $(window).load(function() {
 	$(".loader").removeClass( "loader-active" );
